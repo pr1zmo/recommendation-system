0815d9dd910647408c698be4b80d302287d69f8e
pr1zmo <zaidbassari@gmail.com>
2026-03-06 23:52:09 +0000

    Refactor API key handling and update requirements

--- api/api-get.py
+++ api/api-get.py
@@ -1,14 +1,20 @@
-# 
-
-""" 
 import json
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # The API endpoint URL
-api_url = "https://www.eventbriteapi.com/v3/users/me/?token=MYTOKEN"
+API_KEY = os.getenv('EVENTBRITE_API_KEY')  # Make sure to set your API key in the environment variables
+api_url = f"https://www.eventbriteapi.com/v3/users/me/?token={API_KEY}"
+
+print(api_url)
 
 # Make the GET request
 response = requests.get(api_url)
+
+print(response)
 
 # Check the status code
 if response.status_code == 200:
@@ -16,11 +22,9 @@ if response.status_code == 200:
     data = response.json()
 
     # Access and use the data
-    print(f"Number of people in space: {data['number']}")
-    for person in data['people']:
-        print(f"- {person['name']} on {person['craft']}")
+    # print(f"Number of people in space: {data['number']}")
+    print(data)
 
 else:
     # Handle the error
     print(f"API request failed with status code: {response.status_code}")
-"""
